b9b0fc9d36c74ec3d09881669a295ec104cc9512
larissadantier <larissa_dantier@hotmail.com>
2020-12-12 07:07:18 -0300

    Versão 1.0

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon'
+export default {
+  name: 'App',
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  components:{
+    Pokemon
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0").then(res =>{
+      this.pokemons = res.data.results;
+    })
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <h1>{{num}} {{name | upper}} </h1>
+  <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function(){
+        axios.get(this.url).then(res =>{
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            console.log(this.pokemon)
+        })
+    },
+    data(){
+        return{
+            pokemon: {
+
+            }
+        }
+    },
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+    filters:{
+      upper: function(value){
+          var newName = value[0].toUpperCase() + value.slice(1);
+          return newName;
+      }  
+    }
+}
+</script>
+
+<style>
+
+</style>

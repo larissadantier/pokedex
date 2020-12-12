6ca17197c251fd3a9e81cedf9a293e89672dd688
larissadantier <larissa_dantier@hotmail.com>
2020-12-12 07:55:42 -0300

    Versão final

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <div v-for="(poke,index) in pokemons" :key="index">
+    <div class="column is-half is-offset-one-quarter">
+      <input class="input is-rounded" type="text" name="" id="" placeholder="Buscar Pokémon pelo nome" v-model="buscar"> 
+    <div v-for="(poke,index) in resultadoBusca" :key="index">
       <Pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+     </div>
     </div>
   </div>
 </template>
@@ -13,7 +16,8 @@ export default {
   name: 'App',
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      buscar: ''
     }
   },
   components:{
@@ -23,7 +27,15 @@ export default {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0").then(res =>{
       this.pokemons = res.data.results;
     })
-    
+  },
+  computed:{
+    resultadoBusca: function() {
+      if(this.buscar == '' || this.buscar == ' ') {
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.buscar)
+      }
+    }
   }
 }
 </script>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,8 +1,27 @@
 <template>
-<div>
-  <h1>{{num}} {{name | upper}} </h1>
-  <small>{{url}}</small>
+<div id="pokemon">
+
+    <div class="card">
+    <div class="card-image">
+        <figure class="image is-4by3 image-pokemon">
+        <img :src="currentImg" alt="Placeholder image">
+        </figure>
+    </div>
+    <div class="card-content">
+        <div class="media">
+        <div class="media-content">
+            <p class="title is-4">{{num}} - {{name | upper}}</p>
+            <p class="subtitle is-6">{{pokemon.type}}</p>
+        </div>
+        </div>
+
+        <div class="content">
+            <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar sprite</button>
+        </div>
+
   </div>
+ </div>
+</div>
 </template>
 
 <script>
@@ -14,13 +33,18 @@ export default {
             this.pokemon.type = res.data.types[0].type.name;
             this.pokemon.front = res.data.sprites.front_default;
             this.pokemon.back = res.data.sprites.back_default;
+            this.currentImg = this.pokemon.front
             console.log(this.pokemon)
         })
     },
     data(){
         return{
+            isFront: true,
+            currentImg: '',
             pokemon: {
-
+                type: '',
+                front: '',
+                back: ''
             }
         }
     },
@@ -34,10 +58,29 @@ export default {
           var newName = value[0].toUpperCase() + value.slice(1);
           return newName;
       }  
+    },
+    methods:{
+        mudarSprite: function(){
+            if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.back
+            } else {
+                this.isFront = true;
+                this.currentImg = this.pokemon.front
+            }
+        }
     }
 }
 </script>
 
 <style>
-
+    #pokemon{
+        margin-top: 2%;
+    }
+    .image-pokemon{
+        image-rendering: -moz-crisp-edges;
+        image-rendering: -webkit-crisp-edges;
+        image-rendering: pixelated;
+        image-rendering: crisp-edges;
+    }
 </style>
